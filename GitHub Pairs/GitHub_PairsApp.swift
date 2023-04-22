74a28906cae8fdfc7fe40bd43d4769da0787f946
Christine Morrill <christinenicolemorrill@gmail.com>
2023-04-21 20:06:48 -0400

    Initial Commit

---
+++ GitHub Pairs/GitHub_PairsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_PairsApp.swift
+//  GitHub Pairs
+//
+//  Created by Christine Pitino on 4/21/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_PairsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
